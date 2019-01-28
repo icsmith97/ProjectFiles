Fixed Gradient Descent
Not sure why, but the gradient descent algorithm used was wrong and involved the hessian matrix.

Solvers/gradient_descent_solver.py
@@ -4,23 +4,35 @@ import time
 
 
 class GradientDescentSolver(solver.Solver):
-    def solve(self, grad_f, hessian_f, gamma):
-        xk = self.x0
-        k = 0
+    def solve(self, grad_f, hessian_f, gamma, **kwargs):
+        should_time = kwargs.get('Time', False)
+        repetitions = 1
 
-        grad_f_norm = np.linalg.norm(grad_f(xk))
+        if (should_time):
+            repetitions = 1000
+            start = time.time()
 
-        xs = [xk]
+        for i in range(repetitions):
 
-        start_time = time.time()
 
-        while (grad_f_norm > self.tol) and (k < self.maxIts):
-            xkp1 = xk - gamma * (hessian_f(xk).transpose() @ grad_f(xk))
-            xk = xkp1
+            n = len(self.x0)  # use the starting value to determine the dimension of the problem
+            xk = self.x0
+            k = 0
+
             grad_f_norm = np.linalg.norm(grad_f(xk))
-            k += 1
-            xs.append(xk)
+            xs = [xk]
+
+            start_time = time.time()
+
+            while (grad_f_norm > self.tol) and (k < self.maxIts):
+                xkp1 = xk - gamma * grad_f(xk)
+                xk = xkp1
+                grad_f_norm = np.linalg.norm(grad_f(xk))
+                k += 1
+                xs.append(xk)
 
         self.trace = xs
         self.its = k
-        self.time_taken = time.time() - start_time
+
+        if (should_time):
+            self.time_taken = time.time() - start

Solvers/test1_sphere.py
@@ -23,12 +23,13 @@ def sphere_hessian(x):
     return np.array([[2, 0, 0], [0, 2, 0], [0, 0, 2]])
 
 
-x0 = np.array([2, 2, 2]).T
+x0 = np.array([2, -1, 3])
 max_its = 1000
 tol = 1e-4
+gamma = 1e-2
 
 solver = solver.Solver(x0, max_its, tol)
-comparisons.compare(solver, sphere, sphere_grad, sphere_hessian)
+comparisons.compare(solver, sphere, sphere_grad, sphere_hessian, gamma)
 
-root = np.array([0, 0, 0]).T
+root = np.array([0, 0, 0])
 

Solvers/test2_rosen.py
@@ -24,12 +24,13 @@ def rosenbrock_grad_f(x):
 def rosenbrock_hess_f(x):
     return rosen_hess(x)
 
+x0 = np.asarray([2, 2])
 
-x0 = np.array([2, 2]).T
-tol = 1e-6
+tol = 1e-2
 max_its = 100000
+gamma = 1e-3
 
 solver = solver.Solver(x0, max_its, tol)
-comparisons.compare(solver, rosenbrock_f, rosenbrock_grad_f, rosenbrock_hess_f)
+comparisons.compare(solver, rosenbrock_f, rosenbrock_grad_f, rosenbrock_hess_f, gamma)
 
-root = np.array([1, 1]).T
+root = np.array([1, 1])